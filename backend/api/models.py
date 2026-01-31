3cd370bb6713a1b628f8649a1e1d36b8eb4bc58c
WaliurR029 <mdwaliur63@gmail.com>
2026-01-31 18:26:40 +0600

    api file updated

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,21 @@
 class Task(Documen):
+
+    #core data
     title = StringField(required = True, max_length=200)
     description = StringField()
     category = StringField(choices = ('study', 'work', 'personal', 'shopping', 'other'), default = 'study')
 
-    
+    #Scheduling
+    dead_line=DateTimeField(required = True)
+    estimated_duration = FloatField(required = True )
+    actual_duration = FloatField(default = 0.0)
 
- 
+    #Status
+    is_completed = BooleanField(default = False)
+    created_at = DateTimeField(default = datetime.datetime.now)
 
+    # AI Metadata (For the "Neuro-Symbolic" part)
+    ai_confidence_score = FloatField(default=0.0)
+
+def __str__(self):
+    return self.title
